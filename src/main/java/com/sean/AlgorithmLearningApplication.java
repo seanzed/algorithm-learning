b3cc448d5c9fc1b13e337a72c06de91608911b52
chenxu <seanzed@163.com>
2020-04-06 21:51:13 +0800

    init project

---
+++ src/main/java/com/sean/AlgorithmLearningApplication.java
@@ -0,0 +1,13 @@
+package com.sean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmLearningApplication.class, args);
+	}
+
+}
