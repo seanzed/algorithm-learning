6bf877d1a6b1bb54ae8fd887ff49cf2d3d7eb2e2
chenxu071 <chenxu071@ke.com>
2020-11-19 20:19:01 +0800

    二叉树刷题

---
+++ src/main/java/com/sean/刷题/二叉树/_99_恢复二叉搜索树.java
@@ -0,0 +1,111 @@
+package com.sean.刷题.二叉树;
+
+/**
+ * _99_恢复二叉搜索树
+ * 给你二叉搜索树的根节点 root ，该树中的两个节点被错误地交换。请在不改变其结构的情况下，恢复这棵树。
+ *
+ * 进阶：使用 O(n) 空间复杂度的解法很容易实现。你能想出一个只使用常数空间的解决方案吗？
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,3,null,null,2]
+ * 输出：[3,1,null,null,2]
+ * 解释：3 不能是 1 左孩子，因为 3 > 1 。交换 1 和 3 使二叉搜索树有效。
+ * 示例 2：
+ *
+ *
+ * 输入：root = [3,1,4,null,null,2]
+ * 输出：[2,1,4,null,null,3]
+ * 解释：2 不能在 3 的右子树中，因为 2 < 3 。交换 2 和 3 使二叉搜索树有效。
+ *  
+ *
+ * 提示：
+ *
+ * 树上节点的数目在范围 [2, 1000] 内
+ * -231 <= Node.val <= 231 - 1
+ *
+ *
+ * 链接：https://leetcode-cn.com/problems/recover-binary-search-tree
+ *
+ * @author chenxu
+ * @summary _99_恢复二叉搜索树
+ * @since 2020-11-19 00:13
+ */
+public class _99_恢复二叉搜索树 {
+
+    /**
+     * 上一次遍历过的节点
+     */
+    private TreeNode prev;
+
+    /**
+     * 第一次出错的节点
+     */
+    private TreeNode first;
+
+    /**
+     * 第二次出错的节点
+     */
+    private TreeNode second;
+
+    public void recoverTree(TreeNode root) {
+        TreeNode node = root;
+        while (node != null) {
+            if (node.left != null) {
+                // 找到前驱节点(predecessor)、后继节点(successor)
+                TreeNode pred = node.left;
+                while (pred.right != null && pred.right != node) {
+                    pred = pred.right;
+                }
+
+                if (pred.right == null) {
+                    pred.right = node;
+                    node = node.left;
+                } else { // pred.right == node
+                    find(node);
+                    pred.right = null;
+                    node = node.right;
+                }
+            } else {
+                find(node);
+                node = node.right;
+            }
+        }
+
+        // 交换2个错误节点的值
+        int tmp = first.val;
+        first.val = second.val;
+        second.val = tmp;
+    }
+
+    // 解题错误  @TODO
+    public void recoverTree1(TreeNode root) {
+        findWrongNodes(root);
+        TreeNode tmp = first;
+        first = second;
+        second = tmp;
+    }
+
+    private void findWrongNodes(TreeNode root) {
+        if (root == null) return;
+        findWrongNodes(root.left);
+        find(root);
+        findWrongNodes(root.right);
+    }
+
+    private void find(TreeNode node) {
+        // 出现逆序对
+        if (prev != null && prev.val > node.val) {
+            // 第2个错误节点：最后一个逆序对中较小的那个节点
+            second = node;
+
+            // 第1个错误节点：第一个逆序对中较大的那个节点
+            if (first != null) return;
+            first = prev;
+        }
+        prev = node;
+    }
+}
