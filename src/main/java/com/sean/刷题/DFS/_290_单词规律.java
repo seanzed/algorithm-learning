de615e67a771e57093411d9497ed503fccb5b2db
chenxu071 <chenxu071@ke.com>
2020-12-17 20:23:47 +0800

    DSF刷题

---
+++ src/main/java/com/sean/刷题/DFS/_290_单词规律.java
@@ -0,0 +1,64 @@
+package com.sean.刷题.DFS;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * _290_单词规律
+ * 给定一种规律 pattern 和一个字符串 str ，判断 str 是否遵循相同的规律。
+ *
+ * 这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 str 中的每个非空单词之间存在着双向连接的对应规律。
+ *
+ * 示例1:
+ *
+ * 输入: pattern = "abba", str = "dog cat cat dog"
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入:pattern = "abba", str = "dog cat cat fish"
+ * 输出: false
+ * 示例 3:
+ *
+ * 输入: pattern = "aaaa", str = "dog cat cat dog"
+ * 输出: false
+ * 示例 4:
+ *
+ * 输入: pattern = "abba", str = "dog dog dog dog"
+ * 输出: false
+ *
+ * 链接：https://leetcode-cn.com/problems/word-pattern
+ *
+ * @author chenxu
+ * @summary _290_单词规律
+ * @since 2020-12-16 23:42
+ */
+public class _290_单词规律 {
+
+    public boolean wordPattern(String pattern, String s) {
+        Map<Character, String> map = new HashMap<>();
+        String[] arr = s.split(" ");
+        //如果二者长度都不等，那么根本无法匹配 直接返回false
+        if(pattern.length() != arr.length)
+            return false;
+        for(int i = 0; i < pattern.length(); i++){
+            if(!map.containsKey(pattern.charAt(i))){
+                // 如果不包含key，包含对应的value 直接返回false
+                if(map.containsValue(arr[i])) {
+                    return false;
+                }
+                map.put(pattern.charAt(i), arr[i]);
+            }
+            else{
+                if(!map.get(pattern.charAt(i)).equals(arr[i])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        _290_单词规律 test = new _290_单词规律();
+        System.out.println(test.wordPattern("abba", "dog cat cat dog"));
+    }
+}
---
+++ src/main/java/com/sean/刷题/DFS/_剑指Offer_38_字符串的排列.java
@@ -0,0 +1,77 @@
+package com.sean.刷题.DFS;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * _剑指Offer_38_字符串的排列
+ * 输入一个字符串，打印出该字符串中字符的所有排列。
+ *
+ *  
+ *
+ * 你可以以任意顺序返回这个字符串数组，但里面不能有重复元素。
+ *
+ *  
+ *
+ * 示例:
+ *
+ * 输入：s = "abc"
+ * 输出：["abc","acb","bac","bca","cab","cba"]
+ *  
+ *
+ * 限制：
+ *
+ * 1 <= s 的长度 <= 8
+ *
+ * 链接：https://leetcode-cn.com/problems/zi-fu-chuan-de-pai-lie-lcof
+ *
+ * @author chenxu
+ * @summary _剑指Offer_38_字符串的排列
+ * @since 2020-12-16 23:48
+ */
+public class _剑指Offer_38_字符串的排列 {
+
+    public String[] permutation(String s) {
+        if (s == null) return null;
+        if (s.length() == 0) return new String[0];
+        List<String> list = new ArrayList<>();
+        char[] chars = s.toCharArray();
+        dsf(0, chars, list);
+        String[] result = new String[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+
+    private void dsf(int idx, char[] chars, List<String> list) {
+        // 判断是否结束
+        if (idx == chars.length) {
+            StringBuilder sb = new StringBuilder();
+            for (char c: chars) {
+                sb.append(c);
+            }
+            list.add(sb.toString());
+        }
+
+        for (int i = idx; i < chars.length; i++) {
+            if (isRepeat(chars, idx, i)) continue;
+            swap(chars, idx, i);
+            dsf(idx + 1, chars, list);
+            swap(chars, idx, i);
+        }
+    }
+
+    private Boolean isRepeat(char[] chars, int idx, int i) {
+        for (int j = idx; j < i; j++) {
+            if (chars[j] == chars[i]) return true;
+        }
+        return false;
+    }
+
+    private void swap(char[] chars, int i, int j) {
+        char tmp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = tmp;
+    }
+}
